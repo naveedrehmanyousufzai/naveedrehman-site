1d6f8d78972469f740e8c7717b738a3c3d3167a7
Naveed Rehman Yousufzai <nrehman552@gmail.com>
2026-05-28 17:11:52 +0500

    Clean slate: Removed initial portal drafts

--- sanity/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { tournamentType } from './tournament'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [tournamentType],
-}
